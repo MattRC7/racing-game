a59ccfdc3da5bd3847d34bacf85a1712cbc48d2c
Matthew Cardarelli <thoughtfulgamer@gmx.us>
2018-01-06 14:06:17 -0500

    Refactoring checkpoints/goals pt 1

---
+++ Assets/Checkpoint_Goal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint_Goal : MonoBehaviour {
+
+	public int credit_reward;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Player player = other.GetComponent<Player>();
+		if (player) {
+			player.addCredits(credit_reward);
+		}
+	}
+}
---
+++ Assets/_scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private int credits;
+
+	public void addCredits(int count) {
+		credits += count;
+		Debug.Log(credits);
+	}
+
+	// Use this for initialization
+	void Start () {
+		credits = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

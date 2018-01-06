01f70aeea54f647389e71a28b3839a5c9f3c868d
Matthew Cardarelli <thoughtfulgamer@gmx.us>
2018-01-06 14:48:48 -0500

    Added 2-checkpoint feature

---
+++ Assets/Checkpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+
+	public Checkpoint_Goal next;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.GetComponent<Player>()) {
+			next.gameObject.SetActive(true);
+			Destroy(gameObject);
+		}
+	}
+}

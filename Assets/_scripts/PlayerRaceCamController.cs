8173d7967ac08cc0e7b8c3e7b0d1542f0b3f6378
Matthew Cardarelli <thoughtfulgamer@gmx.us>
2018-01-12 20:49:02 -0500

    Simplified camera

---
+++ Assets/_scripts/PlayerRaceCamController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaceCamController : MonoBehaviour {
+	public GameObject target;
+	public float height;
+	public float back;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = cameraPosition(height, back);
+		transform.rotation = cameraRotation();
+	}
+	
+	Vector3 lookDirection () {
+		return target.transform.forward;
+	}
+	Vector3 cameraPosition (float up, float back) {
+		return target.transform.position - target.transform.forward*back + target.transform.up*height;
+	}
+	Quaternion cameraRotation () {
+		return Quaternion.LookRotation(lookDirection());
+	}
+}

7a61e6d2965cb9d3146c540637b6679a073a104b
Matthew Cardarelli <thoughtfulgamer@gmx.us>
2018-01-30 22:12:18 -0500

    Added basic floating down physics

--- Assets/_scripts/Levitator.cs
+++ Assets/_scripts/Levitator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Levitator : MonoBehaviour {
+	public float max_fall_speed;
 
 	private Rigidbody rb;
 
@@ -13,6 +14,13 @@ public class Levitator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		rb.AddForce(transform.up * Physics.gravity.magnitude);
+		float fall_speed = -Vector3.Project(rb.velocity, Physics.gravity).magnitude;
+		if (fall_speed <= -max_fall_speed) {
+			this.AddAntiGravityForce();
+		}
+	}
+
+	void AddAntiGravityForce() {
+		rb.AddForce(-Physics.gravity);
 	}
 }

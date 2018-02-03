7fb2789e2ef01b7adb1c78af3a4156d1f9cb5624
Matthew Cardarelli <thoughtfulgamer@gmx.us>
2018-02-02 20:40:08 -0500

    Stabilizing roll

--- Assets/_scripts/Levitator.cs
+++ Assets/_scripts/Levitator.cs
@@ -24,6 +24,22 @@ public class Levitator : MonoBehaviour {
 		if (fall_speed < 0 && at_hover_distance) {
 			rb.AddForce(-Vector3.Project(rb.velocity, Physics.gravity), ForceMode.VelocityChange);
 		}
+
+		float current_roll = transform.rotation.z;
+		Debug.Log("roll="+current_roll);
+		if (Mathf.Abs(current_roll) > 0.01f*Mathf.Deg2Rad) {
+			float current_roll_speed = Vector3.Project(rb.angularVelocity, transform.forward).magnitude;
+			Debug.Log("roll_speed="+current_roll_speed);
+			if (current_roll > 0 && current_roll_speed > -1) {
+				rb.AddTorque(-transform.forward);
+			}
+			else if (current_roll < 0 && current_roll_speed < 1) {
+				rb.AddTorque(transform.forward);
+			}
+		}
+		else {
+			rb.AddTorque(-Vector3.Project(rb.angularVelocity, transform.forward), ForceMode.VelocityChange);
+		}
 	}
 
 	bool isAtOrBelowHoverDistance () {

df0830a151ef8d877806699984d045f2d3941ef7
Matthew Cardarelli <thoughtfulgamer@gmx.us>
2018-03-06 20:06:19 -0500

    Able to spawn new hovercraft

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -6,8 +6,22 @@ public class PlayerManager : MonoBehaviour {
 
 	public Player player_component;
 
+	private Player[] players = new Player[4];
+
 	// Use this for initialization
 	void Start () {
-		Instantiate(player_component, Vector3.zero, Quaternion.identity);
+		Instantiate(player_component, new Vector3(3, 4, 3), Quaternion.identity);
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Alpha2)) {
+			Instantiate(player_component, new Vector3(3, 4, -3), Quaternion.identity);
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha3)) {
+			Instantiate(player_component, new Vector3(-3, 4, 3), Quaternion.identity);
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha4)) {
+			Instantiate(player_component, new Vector3(-3, 4, -3), Quaternion.identity);
+		}
 	}
 }

6193d0bb074061a8d0e4ab9cc06cb7e2dc0a3990
Matthew Cardarelli <thoughtfulgamer@gmx.us>
2018-03-06 20:28:40 -0500

    Able to toggle 1-4 players

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -14,20 +14,37 @@ public class PlayerManager : MonoBehaviour {
 	}
 
 	void Update () {
+		if (Input.GetKeyDown(KeyCode.Alpha1)) {
+			RemovePlayer(2);
+			RemovePlayer(3);
+			RemovePlayer(4);
+		}
 		if (Input.GetKeyDown(KeyCode.Alpha2)) {
 			AddPlayer(2, new Vector3(4, 4, -4));
+			RemovePlayer(3);
+			RemovePlayer(4);
 		}
 		if (Input.GetKeyDown(KeyCode.Alpha3)) {
+			AddPlayer(2, new Vector3(4, 4, -4));
 			AddPlayer(3, new Vector3(-4, 4, 4));
+			RemovePlayer(4);
 		}
 		if (Input.GetKeyDown(KeyCode.Alpha4)) {
+			AddPlayer(2, new Vector3(4, 4, -4));
+			AddPlayer(3, new Vector3(-4, 4, 4));
 			AddPlayer(4, new Vector3(-4, 4, -4));
 		}
 	}
 
 	void AddPlayer (int number, Vector3 position) {
 		if (number >= 1 && number <= 4 && !players[number - 1]) {
-			players[number - 1] = Instantiate(player_component, position, Quaternion.identity);
+			players[number - 1] = Instantiate(player_component, position, Quaternion.identity).GetComponent<Player>();
+		}
+	}
+
+	void RemovePlayer (int number) {
+		if (number >= 1 && number <= 4 && players[number - 1]) {
+			Destroy(players[number - 1].gameObject);
 		}
 	}
 }
